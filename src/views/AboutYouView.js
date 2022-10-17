eb47188a8c3154414f0bdedb803f7cb34f25c999
HealthyFoods-JSELabs <90835293+HealthyFoods-JSELabs@users.noreply.github.com>
2022-10-18 00:30:33 +0530

    AboutYouView

--- src/views/AboutYouView.js
+++ src/views/AboutYouView.js
@@ -1,18 +1,43 @@
-import React from "react";
-import AboutYouComponent from "../components/AboutYouComponent/AboutYouComponent";
+import React, {Component} from 'react';
+import {Container, Row} from "reactstrap";
+import {Button} from "react-bootstrap";
 
 
-
-class AboutYouView extends React.Component {
-
+export default class AboutYouView extends Component {
     render() {
+
+        let pageTitle = this.props.pageTitle;
         return (
-            <div className="ChatHistoryView">
-                <AboutYouComponent/>
+
+            <div>
+                <Row style={{margin: 0}}>
+                    <div style={{
+                        backgroundColor: '#003E9B', height: '16vw',
+                        borderBottomLeftRadius: '10vw', borderBottomRightRadius: '10vw',
+                        display: "inline-flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        textAlign: "center"
+                    }}
+                    >
+                        <div style={{
+                            height: '100%',
+                            color: 'white',
+                            fontSize: '4vw',
+                            fontWeight: 'bold',
+                            display: "inline-flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            textAlign: "center",
+                            fontFamily:'sans-serif'
+                        }}>
+                            {pageTitle}
+                        </div>
+                    </div>
+                </Row>
             </div>
+
 
         );
     }
 }
-
-export default AboutYouView;
