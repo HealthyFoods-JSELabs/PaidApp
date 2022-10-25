959da7f32604d7f2c5be8340525b4c79fb882f68
Supun Nilakshana <72870589+supunnilakshana@users.noreply.github.com>
2022-10-26 00:58:54 +0530

    test screens

---
+++ paidapp_mobile/lib/test_screens/testscreen2.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<Data>> fetchData() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums'));
+  if (response.statusCode == 200) {
+    List jsonResponse = json.decode(response.body);
+    return jsonResponse.map((data) => new Data.fromJson(data)).toList();
+  } else {
+    throw Exception('Unexpected error occured!');
+  }
+}
+
+class Data {
+  final int userId;
+  final int id;
+  final String title;
+
+  Data({required this.userId, required this.id, required this.title});
+
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return Data(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+    );
+  }
+}
+
+class TestScreen2 extends StatefulWidget {
+  TestScreen2({Key? key}) : super(key: key);
+
+  @override
+  _TestScreen2State createState() => _TestScreen2State();
+}
+
+class _TestScreen2State extends State<TestScreen2> {
+  late Future<List<Data>> futureData;
+
+  @override
+  void initState() {
+    super.initState();
+    futureData = fetchData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter ListView'),
+      ),
+      body: Center(
+        child: FutureBuilder<List<Data>>(
+          future: futureData,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<Data>? data = snapshot.data;
+              return ListView.builder(
+                  itemCount: data!.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Container(
+                      height: 75,
+                      color: Colors.white,
+                      child: Center(
+                        child: Text(data[index].title),
+                      ),
+                    );
+                  });
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+            // By default show a loading spinner.
+            return CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
+  }
+}
