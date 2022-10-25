3816ee473b619ee9528c17be6b4161e55a222909
Gaya39877 <gayathrirasangikahw@gmail.com>
2022-10-25 20:01:54 +0530

    commit03

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React for hacktoberfest-2022 
         </a>
       </header>
     </div>
