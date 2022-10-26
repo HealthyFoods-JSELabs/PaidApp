c388353b6d26627bc90646a58aaaec7bc1b6ea05
sHaViNX <88244172+sHaViNX@users.noreply.github.com>
2022-10-27 00:27:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
+function mainDpFunction() {
+    this.getData = function (fname) {
+        if (fname === "Jason") return 20000;
+        else if (fname === "DEF") return 5000;
+        else return 0;
+    };
+}
+  
+function proxyDpFunction() {
+    let object_1 = new mainDpFunction();
+    let result_object = {};
+    return {
+        getData: function (fname) {
+            if (!result_object[fname]) {
+                result_object[fname] = object_1.getData(fname);
+            }
+            console.log(fname + ": " + result_object[fname]);
+            return result_object[fname];
+        },
+    };
+}
+  
+function mainFunction() {
+    let main_object = new proxyDpFunction();
+    main_object.getData("Jason");
+    main_object.getData("Samoa");
+    main_object.getData("Hightenford");
+}
+  
+mainFunction();
 
 function App() {
   return (
