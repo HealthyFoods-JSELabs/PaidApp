92e155bf7ab68e4f00cd299fe0c47e0a42eca627
SANDARUWANRMT <rmtsandaruwan@std.appsc.sab.ac.lk>
2022-10-29 22:43:02 +0530

    returning all the triplets

--- src/App.test.js
+++ src/App.test.js
@@ -45,3 +45,49 @@ for (let i = ln; i <= hn; i++) {
         console.log(i);
     }
 }
+
+/*
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+*/
+
+const arr = parseInt(prompt('Enter array: '));
+console.log(threeSum(arr));
+
+var threeSum = function(nums) {
+	var rtn = [];
+	if (nums.length < 3) {
+		return rtn;
+	}
+	nums = nums.sort(function(a, b) {
+		return a - b;
+	});
+	for (var i = 0; i < nums.length - 2; i++) {
+		if (nums[i] > 0) {
+			return rtn;
+		}
+		if (i > 0 && nums[i] == nums[i - 1]) {
+			continue;
+		}
+		for (var j = i + 1, k = nums.length - 1; j < k;) {
+			if (nums[i] + nums[j] + nums[k] === 0) {
+				rtn.push([nums[i], nums[j], nums[k]]);
+				j++;
+				k--;
+				while (j < k && nums[j] == nums[j - 1]) {
+					j++;
+				}
+				while (j < k && nums[k] == nums[k + 1]) {
+					k--;
+				}
+			} else if (nums[i] + nums[j] + nums[k] > 0) {
+				k--;
+			} else {
+				j++;
+			}
+		}
+	}
+	return rtn;
+};
