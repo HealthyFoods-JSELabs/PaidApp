e80eba74aa3db64853dc02bb3049c0afd78579e7
Hashan Ranasinghe <73054932+hashanranasinghe@users.noreply.github.com>
2022-10-29 12:27:34 +0530

    Update Barchart.js

--- src/components/Barchart/Barchart.js
+++ src/components/Barchart/Barchart.js
@@ -5,6 +5,27 @@ import {
 import { Form, Button } from 'react-bootstrap';
 import CardText from "reactstrap/es/CardText";
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/index.html");
+});
+app.post("/",function(req,res){
+    var height = Number(req.body.height);
+    var mass = Number(req.body.mass);
+    var result = (height/(mass*mass));
+    res.send("Your BMI is "+ result);
+})
+
+app.listen(3000,function(){
+    console.log("The server is running on 3000.");
+})
+
+
 
 class Barchart extends React.Component {
     state = {
